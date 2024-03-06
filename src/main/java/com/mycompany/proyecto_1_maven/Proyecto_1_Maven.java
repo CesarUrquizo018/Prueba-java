429443627064a7eec74358c034ca9cb1377a2481
CesarUrquizo018 <cesar.urquizo@unmsm.edu.pe>
2024-03-05 23:36:07 -0500

    crear proyecto

---
+++ src/main/java/com/mycompany/proyecto_1_maven/Proyecto_1_Maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_1_maven;
+
+/**
+ *
+ * @author cesar
+ */
+public class Proyecto_1_Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
